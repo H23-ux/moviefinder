b06cd553ba44113b8732f07b5b87dae18c7cfe42
H-23ux <harshenisuresh5@gmail.com>
2026-05-05 16:12:30 +0530

    first commit

---
+++ src/main/java/com/example/moviefinder/Entity/Profile.java
@@ -0,0 +1,40 @@
+package com.example.moviefinder.Entity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Profile {
+    private String username;
+    private int age;
+    private String gender;
+
+    public Profile(String username, int age, String gender) {
+        this.username = username;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+}
